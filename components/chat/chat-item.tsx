3c3d67ebc44baea1a386e0546b15ec78ce5a0348
Jack <jac.dragonchild@gmail.com>
2023-10-18 21:09:08 +1100

    video and audio added

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -13,6 +13,7 @@ import { Edit, FileIcon, ShieldAlert, ShieldCheck, Trash } from "lucide-react";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { cn } from "@/lib/utils";
+import { useRouter, useParams } from "next/navigation"
 
 import {
     Form,
@@ -22,6 +23,7 @@ import {
 } from "@/components/ui/form"
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ChatItemProps {
     id: string;
@@ -61,7 +63,17 @@ export const ChatItem = ({
     socketQuery
 }: ChatItemProps) => {
     const [isEditing, setIsEditing] = useState(false)
-    const [isDeleting, setIsDeleting] = useState(false)
+    const { onOpen } = useModal()
+    const params = useParams()
+    const router = useRouter()
+
+    const onMemberClick = () => {
+      if (member.id === currentMember.id) {
+        return
+      }
+
+      router.push(`/servers/${params?.serverId}/conversations/${member.id}`)
+    }
 
     useEffect(() => {
         const handleKeyDown = (event: any) => {
@@ -118,17 +130,24 @@ export const ChatItem = ({
     const isPDF = fileType === "pdf" && fileUrl;
     const isImage = !isPDF && fileUrl;
 
+    console.log("Member object:", member)
+    console.log("Profile object:", member.profile)
+
   return (
     <div className="relative group flex items-center hover:bg-black/5
     p-4 transition w-full">
         <div className="group flex gap-x-2 items-start w-full">
-            <div className="cursor-pointer hover:drop-shadow-md transition">
+            <div 
+            onClick={onMemberClick}
+            className="cursor-pointer hover:drop-shadow-md transition">
                 <UserAvatar src={member.profile.imageUrl} />
             </div>
             <div className="flex flex-col w-full">
                 <div className="flex items-center gap-x-2">
                     <div className="flex items-center mb-1">
-                        <p className="font-semibold text-sm hover:underline cursor-pointer">
+                        <p 
+                        onClick={onMemberClick}
+                        className="font-semibold text-sm hover:underline cursor-pointer">
                             {member.profile.name}
                         </p>
                         <ActionTooltip label={member.role}>
@@ -232,6 +251,10 @@ export const ChatItem = ({
                 )}
                     <ActionTooltip label="Delete">
                         <Trash 
+                        onClick={() => onOpen("deleteMessage", { 
+                            apiUrl: `${socketUrl}/${id}`,
+                            query: socketQuery,
+                         })}
                         className="cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:hover:text-zinc-300 
                         transition"
                         />
